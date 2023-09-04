823e2e8639bb0a1e3701ed283d10cacadc228354
jeanmmlima <jeanmariomlima@gmail.com>
2023-09-04 08:48:04 -0300

    data dinamica

--- f02_todolist/lib/components/form_tarefa.dart
+++ f02_todolist/lib/components/form_tarefa.dart
@@ -1,21 +1,44 @@
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
-class FormTarefa extends StatelessWidget {
-  Function(String) onSubmit;
+class FormTarefa extends StatefulWidget {
+  Function(String, DateTime) onSubmit;
   FormTarefa(this.onSubmit);
 
   @override
-  Widget build(BuildContext context) {
-    final _tarefaController = TextEditingController();
+  State<FormTarefa> createState() => _FormTarefaState();
+}
 
+class _FormTarefaState extends State<FormTarefa> {
+  final _tarefaController = TextEditingController();
+  DateTime _dataSelecionada = DateTime.now();
+  @override
+  Widget build(BuildContext context) {
     _submitForm() {
+      final titulo = _tarefaController.text;
       if (_tarefaController.text.isEmpty) {
         return;
       }
       //passando dado para componente pai
-      onSubmit(_tarefaController.text);
+      widget.onSubmit(titulo, _dataSelecionada);
+    }
+
+    _showDatePicker() {
+      showDatePicker(
+              context: context,
+              initialDate: DateTime.now(),
+              firstDate: DateTime.now(),
+              lastDate: DateTime(2024))
+          .then((pickedDate) {
+        if (pickedDate == null) {
+          return;
+        }
+        setState(() {
+          _dataSelecionada = pickedDate;
+        });
+      });
     }
 
     return Container(
@@ -25,8 +48,23 @@ class FormTarefa extends StatelessWidget {
             controller: _tarefaController,
             decoration: const InputDecoration(labelText: 'nova tarefa...'),
           ),
-          ElevatedButton(
-              onPressed: _submitForm, child: Text('Cadastrar tarefa'))
+          Container(
+            child: Row(
+              children: <Widget>[
+                Expanded(
+                  child: Text(
+                      'Data selecionada ${DateFormat('dd/mm/yyyy').format(_dataSelecionada)}'),
+                ),
+                TextButton(
+                    onPressed: _showDatePicker, child: Text('Selecionar data'))
+              ],
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: ElevatedButton(
+                onPressed: _submitForm, child: Text('Cadastrar tarefa')),
+          )
         ],
       ),
     );
