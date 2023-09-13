39bcf714e3c3bca85509d76db58e7979138fa01b
jeanmmlima <jeanmariomlima@gmail.com>
2023-09-13 08:43:33 -0300

    drawer

--- f03_lugares/lib/main.dart
+++ f03_lugares/lib/main.dart
@@ -2,6 +2,7 @@ import 'dart:ui';
 
 import 'package:f03_lugares/screens/countries_places_screen.dart';
 import 'package:f03_lugares/screens/place_detail_screen.dart';
+import 'package:f03_lugares/screens/settings_screen.dart';
 import 'package:f03_lugares/utils/app_routes.dart';
 import 'package:flutter/material.dart';
 
@@ -15,21 +16,24 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'PlacesToGo',
       theme: ThemeData(
-          colorScheme: ThemeData()
-              .colorScheme
-              .copyWith(primary: Colors.purple, secondary: Colors.amber),
-          fontFamily: 'Raleway',
-          canvasColor: Color.fromRGBO(255, 254, 229, 1),
-          textTheme: ThemeData.light().textTheme.copyWith(
-                  headline6: TextStyle(
+        colorScheme: ThemeData()
+            .colorScheme
+            .copyWith(primary: Colors.purple, secondary: Colors.amber),
+        fontFamily: 'Raleway',
+        canvasColor: Color.fromRGBO(255, 254, 229, 1),
+        textTheme: ThemeData.light().textTheme.copyWith(
+              headline6: TextStyle(
                 fontSize: 20,
                 fontFamily: 'RobotoCondensed',
-              ))),
+              ),
+            ),
+      ),
       home: CountriesScreen(),
       //initialRoute: '/',
       routes: {
         AppRoutes.COUNTRY_PLACES: (ctx) => CountryPlacesScreen(),
         AppRoutes.PLACES_DETAIL: (ctx) => PlaceDetailScreen(),
+        AppRoutes.SETTINGS: (ctx) => SettingsScreen(),
       },
     );
   }
